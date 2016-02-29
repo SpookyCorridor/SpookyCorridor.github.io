8bfba8be8a3fa179748be3c27db317b578f50f6d
Dylan <invision33@gmail.com>
2016-02-29 10:39:57 -0600

    add contact page

--- main.js
+++ main.js
@@ -17,44 +17,44 @@
 			$('.close').toggle(); 
 		})
 
-var yoyo_tween = TweenMax.to('#yoyo-animation', 1, {
-	transform: 'scale(2)',
-	ease: Cubic.easeOut,
-	repeat: -1,
-	yoyo: true
-}); 
+// var yoyo_tween = TweenMax.to('#yoyo-animation', 1, {
+// 	transform: 'scale(2)',
+// 	ease: Cubic.easeOut,
+// 	repeat: -1,
+// 	yoyo: true
+// }); 
 
-var controller = new ScrollMagic.Controller(); 
+// var controller = new ScrollMagic.Controller(); 
 
-var scene1 = new ScrollMagic.Scene({
-  triggerElement: "#pinned-trigger1", // point of execution
-  duration: $(window).height() - 100, // pin element for the window height - 1
-  triggerHook: 0, // don't trigger until #pinned-trigger1 hits the top of the viewport
-  reverse: true // allows the effect to trigger when scrolled in the reverse direction
-})
-.setPin("#pinned-element1") // the element we want to pin
-
-
-var fadein_tween = TweenMax.to('#fadein-trigger > div', .375, {
-	opacity: 1
-});
-
-var fadein_scene = new ScrollMagic.Scene({
-  triggerElement: '#fadein-trigger',
-  reverse: true,
-  duration: "50%"
-})
-.setTween(fadein_tween)
+// var scene1 = new ScrollMagic.Scene({
+//   triggerElement: "#pinned-trigger1", // point of execution
+//   duration: $(window).height() - 100, // pin element for the window height - 1
+//   triggerHook: 0, // don't trigger until #pinned-trigger1 hits the top of the viewport
+//   reverse: true // allows the effect to trigger when scrolled in the reverse direction
+// })
+// .setPin("#pinned-element1") // the element we want to pin
 
 
-var yoyo_scene = new ScrollMagic.Scene({
-	triggerElement: '#yoyo-trigger'
-})
-.setTween(yoyo_tween); 
+// var fadein_tween = TweenMax.to('#fadein-trigger > div', .375, {
+// 	opacity: 1
+// });
 
-controller.addScene([
-	yoyo_scene,
-	scene1,
-	fadein_scene
-]); 
+// var fadein_scene = new ScrollMagic.Scene({
+//   triggerElement: '#fadein-trigger',
+//   reverse: true,
+//   duration: "50%"
+// })
+// .setTween(fadein_tween)
+
+
+// var yoyo_scene = new ScrollMagic.Scene({
+// 	triggerElement: '#yoyo-trigger'
+// })
+// .setTween(yoyo_tween); 
+
+// controller.addScene([
+// 	yoyo_scene,
+// 	scene1,
+// 	fadein_scene
+// ]); 
 
